281d6bb34d0c5d27ac1641bd66110f409be2ff6a
Kushboo003 <khushboo@nagarro.com>
2022-12-31 04:41:10 +0530

    First commit

---
+++ TestProject/src/strings/string.java
@@ -0,0 +1,14 @@
+package strings;
+
+public class string {
+	
+	public static void main(String[] args) {
+	    String name = "A quick brown fox jumps over the lazy dog" ;
+		System.out.println(name.indexOf("d"));
+		System.out.println(name.charAt(11));
+		System.out.println(name.substring(5));
+		System.out.println(name.toUpperCase());
+			}
+
+
+}
